810b82b8079c8f7b7e128a30a7646c27f53b1dbd
Ivan Lopez <ialmendra@infobae.com>
2025-05-03 22:15:20 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioUno/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace EjercicioUno
+{
+    partial class frmEjercicioUno
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEjercicioUno));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lstPacientes = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnAgregarDespues = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Direccion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 262);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefono";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(27, 40);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(124, 20);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(27, 94);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(124, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(27, 153);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(124, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(28, 216);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(124, 20);
+            this.txtDireccion.TabIndex = 8;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(28, 278);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(124, 20);
+            this.txtTelefono.TabIndex = 9;
+            // 
+            // lstPacientes
+            // 
+            this.lstPacientes.FormattingEnabled = true;
+            this.lstPacientes.Location = new System.Drawing.Point(414, 24);
+            this.lstPacientes.Name = "lstPacientes";
+            this.lstPacientes.Size = new System.Drawing.Size(312, 277);
+            this.lstPacientes.TabIndex = 10;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(231, 40);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(126, 20);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(231, 94);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(126, 20);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(231, 153);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(126, 20);
+            this.btnActualizar.TabIndex = 13;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnAgregarDespues
+            // 
+            this.btnAgregarDespues.Location = new System.Drawing.Point(231, 216);
+            this.btnAgregarDespues.Name = "btnAgregarDespues";
+            this.btnAgregarDespues.Size = new System.Drawing.Size(126, 20);
+            this.btnAgregarDespues.TabIndex = 14;
+            this.btnAgregarDespues.Text = "Agregar despues";
+            this.btnAgregarDespues.UseVisualStyleBackColor = true;
+            this.btnAgregarDespues.Click += new System.EventHandler(this.btnAgregarDespues_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(231, 278);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(126, 20);
+            this.btnMostrar.TabIndex = 15;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // frmEjercicioUno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 332);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnAgregarDespues);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstPacientes);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmEjercicioUno";
+            this.Text = "Ejercicio Uno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.ListBox lstPacientes;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnAgregarDespues;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ EjercicioUno/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace EjercicioUno
+{
+    public partial class frmEjercicioUno : Form
+    {
+        public frmEjercicioUno()
+        {
+            InitializeComponent();
+        }
+
+        private List<Paciente> listPaciente = new List<Paciente>();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MostrarLista()
+        {
+            lstPacientes.Items.Clear();
+            foreach (var paciente in listPaciente)
+            {
+                lstPacientes.Items.Add(paciente);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Paciente nuevoPaciente = new Paciente
+            {
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Apellido = txtApellido.Text,
+                Direccion = txtDireccion.Text,
+                Telefono = txtTelefono.Text
+            };
+
+            listPaciente.Add(nuevoPaciente);
+            MostrarLista();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string curItem = lstPacientes.SelectedItem.ToString();
+            int index = lstPacientes.FindString(curItem);
+            for (int i = 0; i < listPaciente.Count; i++)
+            {
+                if (i == index)
+                {
+                    listPaciente.RemoveAt(i);
+                }
+            }
+            MostrarLista();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string curItem = lstPacientes.SelectedItem.ToString();
+            int index = lstPacientes.FindString(curItem);
+
+            if(index >= 0)
+            {
+                listPaciente[index].Codigo = txtCodigo.Text;
+                listPaciente[index].Nombre = txtNombre.Text;
+                listPaciente[index].Apellido = txtApellido.Text;
+                listPaciente[index].Direccion = txtDireccion.Text;
+                listPaciente[index].Telefono = txtTelefono.Text;
+                MostrarLista();
+            }
+        }
+
+        private void btnAgregarDespues_Click(object sender, EventArgs e)
+        {
+            string curItem = lstPacientes.SelectedItem.ToString();
+            int index = lstPacientes.FindString(curItem);
+            Paciente nuevoPaciente = new Paciente
+            {
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Apellido = txtApellido.Text,
+                Direccion = txtDireccion.Text,
+                Telefono = txtTelefono.Text
+            };
+            listPaciente.Insert(index + 1, nuevoPaciente);
+            MostrarLista();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            MostrarLista();
+        }
+    }
+}
